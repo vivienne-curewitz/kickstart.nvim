d8053e2cb569fc406c6ff07573442e33aaa7b919
vivienne <jaycie.curewitz@gmail.com>
2026-04-07 16:39:32 +0200

    added transparent nvim

--- init.lua
+++ init.lua
@@ -883,6 +883,7 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
